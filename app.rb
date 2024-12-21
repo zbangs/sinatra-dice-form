fed31513a66aea3bbeb576e546b6e66c645eb029
Zackery Bangs <106060493+zbangs@users.noreply.github.com>
2024-12-21 23:01:49 +0000

    Enable functionality for project site

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
+  erb(:process_roll)
 end
